eb850d448880804982b50df199e18f1a5d30c1cc
chetat <yekuwilfred@gmail.com>
2020-06-02 21:40:27 +0100

    Complete Email implementation

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage,\
     PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -11,21 +13,31 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 Posts in each page
-    page = request.GET.get('page')
-
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        #if page is not Int, Deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'posts': posts,
-                                                   'page': page})
+def post_share(request, post_id):
+    #Retrieve post by Id
+    sent = False
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        #Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #Form fields Valid
+            cleaned_data = form.cleaned_data
+            #Send Email
+            post_url  = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cleaned_data['name']} Recommends you read "\
+                       f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cleaned_data['name']} \'s comments: {cleaned_data['comments']}"
+            
+            send_mail(subject, message, 'yeku.wilfred@yahoo.com', [cleaned_data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
 
 
 def post_detail(request, year, month, day, post):
--- blogger/settings.py
+++ blogger/settings.py
@@ -114,6 +114,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
